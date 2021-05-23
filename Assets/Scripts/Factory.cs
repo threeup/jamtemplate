633dee9d4b890188516da6b1196fc4a651a8a5f6
Sheridan Thirsk <threeup@gmail.com>
2021-05-23 12:40:39 -0700

    factory

---
+++ Assets/Scripts/Factory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factory : LazySingletonBehaviour<Factory>
+{
+    public GameObject ProtoCube;
+    public GameObject ProtoCapsule;
+    public GameObject ProtoCone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public GameObject SpawnCube(Vector3 pos)
+    {
+        GameObject go = GameObject.Instantiate(ProtoCube, pos, Quaternion.identity);
+        return go;
+    }
+
+    public Agent SpawnAgent(Vector3 pos)
+    {
+        GameObject go = GameObject.Instantiate(ProtoCapsule, pos, Quaternion.identity);
+        Agent agent = go.AddComponent<Agent>();
+        return agent;
+    }
+
+    public Prop SpawnProp(Vector3 pos)
+    {
+        GameObject go = GameObject.Instantiate(ProtoCone, pos, Quaternion.identity);
+        Prop prop = go.AddComponent<Prop>();
+        return prop;
+    }
+
+}
---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    public Camera mainCamera;
+    private Program program;
+    private Boss boss;
+    private Factory factory;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        program = Program.Instance;
+        boss = Boss.Instance;
+        factory = Factory.Instance;
+        mainCamera = Camera.main;
+        mainCamera.transform.position = new Vector3(4,0,10);
+        factory.SpawnAgent(new Vector3(5,5,5));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
